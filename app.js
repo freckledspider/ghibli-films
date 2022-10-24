5099ce3d99381252fb98a8f1728ce681cec78c7e
bri <bcamesi@gmail.com>
2022-10-24 12:59:01 -0700

    Rendering data from API

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 // Studio Ghibli Film Database
 // ------------------------------------------------------
 
+// testing to get data
+
 const getGhibliData = () => {
 
 		const apiCall = `https://ghibliapi.herokuapp.com/films`;
@@ -10,28 +12,62 @@ const getGhibliData = () => {
 			.then(response => response.json())
 			.then(data => {
 				console.log(data)
-				const title = data[0].title
 	;})
 	};
 
 	
 getGhibliData()
 
+// testing to loop data
+
+// const loopData = () => {
+
+// 	let apiCall = `https://ghibliapi.herokuapp.com/films`;
+
+// 	fetch(apiCall)
+// 		.then(response => response.json())
+// 		.then(data => {
+// 			for(let i = 0; i < data.length; i++) {
+// 				let obj = data[i];
+			
+// 				console.log(obj.title);
+// 			}
+			
+// ;})
+// };
+
+// loopData()
+
+
+
+// ------------------------------------------------------
+
+// function to loop each movie and create an element for each
+
 const loopData = () => {
 
 	let apiCall = `https://ghibliapi.herokuapp.com/films`;
-
+  
 	fetch(apiCall)
-		.then(response => response.json())
-		.then(data => {
-			for(let i = 0; i < data.length; i++) {
-				let obj = data[i];
-			
-				console.log(obj.title);
-			}
-			
-;})
-};
+	  .then(response => response.json())
+	  .then(data => {
+		data.forEach(film => {
+		  // calling createElement that takes data and id of element in which data will append. For each film, create an element.
+		  createElement(film, "#main");
+		})
+	  })
+  };
+  
+  loopData()
 
-loopData()
+// in each created element, data is rendered for each film
 
+  function createElement(data, id) {
+	const film = document.querySelector(id);
+	let filmCard = `<div class="card">
+	<h2>${data.title}</h2>
+	<img src="${data.image}"/>
+	<p>${data.description}</p>
+	</div>`;
+	film.innerHTML += filmCard;
+  }
