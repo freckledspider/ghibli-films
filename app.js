c127e12251276aec8eb50b43ab75a30fb8192a75
bri <bcamesi@gmail.com>
2022-10-24 10:22:36 -0700

    Figuring out the loop

--- app.js
+++ app.js
@@ -11,9 +11,27 @@ const getGhibliData = () => {
 			.then(data => {
 				console.log(data)
 				const title = data[0].title
-				main.innerHTML = `<h3>${title}</h3>`
 	;})
 	};
 
 	
 getGhibliData()
+
+const loopData = () => {
+
+	let apiCall = `https://ghibliapi.herokuapp.com/films`;
+
+	fetch(apiCall)
+		.then(response => response.json())
+		.then(data => {
+			for(let i = 0; i < data.length; i++) {
+				let obj = data[i];
+			
+				console.log(obj.title);
+			}
+			
+;})
+};
+
+loopData()
+
